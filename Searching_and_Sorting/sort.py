38b2f46bf743f4f8ed3763326014c172e4093bc9
hayley_mathews <hmathews.tulane@gmail.com>
2016-09-12 13:10:30 -0500

    merge and quick sort

--- Searching_and_Sorting/sort.py
+++ Searching_and_Sorting/sort.py
@@ -5,7 +5,7 @@ class Sort:
     """
     various sorts
     """
-
+    @staticmethod
     def bubble_sort(values):
         """
         iterate through values and bubble largest value to end O(n**2)
@@ -13,9 +13,10 @@ class Sort:
         for a in range(len(values) - 1):
             for b  in range(len(values[:-a])):
                 if values[b] > values[b + 1]:
-                    values[b], values[b + 1] = values[b+1], values[b]
+                    values[b], values[b + 1] = values[b + 1], values[b]
         return values
 
+    @staticmethod
     def selection_sort(values):
         """
         iterate through values selecting smallest and moving to front O(n**2)
@@ -29,8 +30,58 @@ class Sort:
                 values[i], values[smallest_index] = values[smallest_index], values[i]
         return values
 
+    @staticmethod
     def insertion_sort(values):
         """
         iterate through values and insert value in proper position O(n**2)
         """
-        pass
+        for i in range(1, len(values)):
+            j = i
+            while j > 0 and values[j] < values[j - 1]:
+                values[j], values[j - 1] = values[j - 1], values[j]
+                j -= 1
+        return values
+
+    @staticmethod
+    def merge_sort(values):
+        """
+        divide list in 2(left, right) until each list is only 1 item long, and thus sorted
+        then merge left and right together into a sorted list O(nlogn)
+        """
+        if len(values) <= 1:
+            return values
+        mid = len(values) // 2
+        left = Sort.merge_sort(values[:mid])
+        right = Sort.merge_sort(values[mid:])
+        l, r = 0, 0
+        for i in range(len(values)):
+            left_value = left[l] if l < len(left) else None
+            right_value = right[r] if r < len(right) else None
+            if (left_value and right_value
+                and  left_value < right_value) or right_value is None:
+                values[i] = left_value
+                l += 1
+            elif (left_value and right_value
+                  and  left_value > right_value) or left_value is None:
+                values[i] = right_value
+                r += 1
+        return values
+
+    @staticmethod
+    def quick_sort(values):
+        """
+        """
+        if len(values) > 1:
+            pivot = len(values) // 2
+            smaller = []
+            larger = []
+            for i, value in enumerate(values):
+                if i != pivot:
+                    if value < values[pivot]:
+                        smaller.append(value)
+                    else:
+                        larger.append(value)
+            Sort.quick_sort(smaller)
+            Sort.quick_sort(larger)
+            values[:] = smaller + [values[pivot]] + larger
+        return values
