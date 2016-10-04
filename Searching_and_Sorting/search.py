f1d0df0175b9578ad21c2b730e48f0c1e6578b08
hayley_mathews <hmathews.tulane@gmail.com>
2016-10-04 10:52:10 -0500

    quick_select and doctests

--- Searching_and_Sorting/search.py
+++ Searching_and_Sorting/search.py
@@ -2,6 +2,7 @@
 linear search and binary search, """
 
 import collections
+import random
 
 class Search:
     """
@@ -13,6 +14,9 @@ class Search:
         """
         iterate through input values looking for target O(n)
         note: the pythonic way to do linear search is to use 'in' operator
+        >>> values = [1, 4, 3, 2, 5]
+        >>> Search.linear_search(values, 4)
+        True
         """
         try:
             for value in values:
@@ -27,6 +31,9 @@ class Search:
         """
         iterate through presorted input values looking for target O(n)
         if value larger than target, break
+        >>> values = [1, 2, 3, 4, 5]
+        >>> Search.sorted_linear_search(values, 4)
+        True
         """
         # assert iterable, "input is not iterable"
         for value in values:
@@ -39,6 +46,9 @@ class Search:
     def binary_search(values, target):
         """
         iterate through presorted input values looking for target O(logn)
+        >>> values = [1, 2, 3, 4, 5]
+        >>> Search.binary_search(values, 4)
+        True
         """
         low = 0
         high = len(values) - 1
@@ -51,3 +61,25 @@ class Search:
             else:
                 low = mid_point + 1
         return False
+
+    @staticmethod
+    def quick_select(values, k):
+        """
+        find kth smallest item in values O(n)
+        >>> values = [1, 4, 3, 2, 5]
+        >>> Search.quick_select(values, 2)
+        2
+        """
+        if len(values) == 1:
+            return values[0]
+        pivot = random.choice(values)
+        smaller = [x for x in values if x < pivot]
+        larger = [x for x in values if x > pivot]
+        equal = [x for x in values if x == pivot]
+        if k <= len(smaller):
+            return Search.quick_select(smaller, k)
+        elif k <= len(smaller) + len(equal):
+            return pivot
+        else:
+            j = k - len(smaller) - len(equal)
+            return Search.quick_select(larger)
