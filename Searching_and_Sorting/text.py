32b2b08d47db6eab700c3299d7390bb5323f80e0
hayley_mathews <hmathews.tulane@gmail.com>
2016-10-05 16:39:32 -0500

    boyer moore for text searching

--- Searching_and_Sorting/text.py
+++ Searching_and_Sorting/text.py
@@ -23,3 +23,30 @@ class Text:
             if k == m:
                 return index
         return -1
+
+    @staticmethod
+    def find_pattern_boyer_moore(text, pattern):
+        """
+        boyer moore algorithm to find first index of pattern in text O(nm)
+        >>> text, pattern = 'atcggctatt', 'tat'
+        >>> Text.find_pattern_boyer_moore(text, pattern)
+        6
+        """
+        n, m = len(text), len(pattern)
+        if m == 0:
+            return 0
+        last = {pattern[x]: x for x in range(m)}
+        i = m - 1
+        k = m - 1
+        while i < n:
+            if text[i] == pattern[k]:
+                if k == 0:
+                    return i
+                else:
+                    i -= 1
+                    k -= 1
+            else:
+                j = last.get(text[i], -1)
+                i += m - min(k, j + 1)
+                k = m - 1
+        return -1
