528463dd63c246aedb6a4e6dd65dc4fa8eab08ac
hayley_mathews <hmathews.tulane@gmail.com>
2016-10-06 12:42:15 -0500

    graphs on graphs on graphs yo

--- Graphs/_graph_abstract.py
+++ Graphs/_graph_abstract.py
@@ -6,46 +6,97 @@ class Graph(ABC):
     """
     abstract class representing Graph
     """
-    @property
-    def edges(self):
-        pass
+    class Vertex:
+        """
+        vertex class for Graph
+        """
+        def __init__(self, value):
+            self.value = value
+            self.visited = False
+            self.hash = None
 
-    @property
-    def vertices(self):
-        pass
+        def __eq__(self, other):
+            if not isinstance(other, Graph.Vertex):
+                return False
+            return self.value == other.value
 
-    @abstractmethod
-    def __getitem__(self, vertex):
-        """
-        get value of vertex
-        """
-        pass
+        def __repr__(self):
+            return "Vertex: " + str(self.value)
 
-    @abstractmethod
-    def __setitem__(self, vertex, value):
-        """
-        set value of vertex
-        """
-        pass
+        def __hash__(self):
+            if self.hash is None:
+                try:
+                    self.hash = hash(self.value)
+                except TypeError as e:
+                    raise e
+            return self.hash
 
-    @abstractmethod
-    def add_edge(self, edge):
+        def visit(self):
+            """
+            set visited flag to true and return previous value
+            """
+            old_visited = self.visited
+            self.visited = True
+            return old_visited
+
+        def reset(self):
+            """
+            reset visited flag
+            """
+            self.visited = False
+
+    class Edge:
         """
-        add edge to Graph
+        edge class for Graph
         """
-        pass
+        def __init__(self, here, there, label=None, directed=False):
+            self.here = here
+            self.there = there
+            self.label = label
+            self.directed = directed
+            self.visited = False
+            self.hash = None
+
+        def visit(self):
+            """
+            set visited flag to true and return previous value
+            """
+            old_visited = self.visited
+            self.visited = True
+            return old_visited
+
+        def reset(self):
+            """
+            reset visited flag
+            """
+            self.visited = False
+
+        def __eq__(self, other):
+            if not isinstance(other, Graph.Edge):
+                return False
+            if (self.here == other.here) and (self.there == other.there):
+                return True
+            if not self.directed:
+                if (self.here == other.there) and (self.there == other.here):
+                    return True
+            return False
+
+        def __hash__(self):
+            if self.hash is None:
+                try:
+                    self.hash = hash(self.here) + hash(self.there) + hash(self.label)
+                except TypeError as e:
+                    raise e
+            return self.hash
+
+        def __repr__(self):
+            string = "Edge " + str(self.label) + ": " + str(self.here) + " " + str(self.there)
+            return string
 
     @abstractmethod
     def add_vertex(self, vertex):
         """
         add vertex to Graph
-        """
-        pass
-
-    @abstractmethod
-    def delete_edge(self, edge):
-        """
-        delete edge from Graph
         """
         pass
 
@@ -57,14 +108,126 @@ class Graph(ABC):
         pass
 
     @abstractmethod
+    def get_vertex(self, vertex):
+        """
+        get value of vertex
+        """
+        pass
+
+    @abstractmethod
+    def add_edge(self, here, there, label):
+        """
+        add edge to Graph
+        """
+        pass
+
+    @abstractmethod
+    def delete_edge(self, here, there):
+        """
+        delete edge from Graph
+        """
+        pass
+
+    @abstractmethod
+    def get_edge(self, here, there):
+        """
+        get edge from here to there
+        """
+        pass
+
+    @abstractmethod
+    def __contains__(self, vertex):
+        """
+        check if vertex in graph
+        """
+        pass
+
+    @abstractmethod
+    def contains_edge(self, here, there):
+        """
+        check if edge from here to there exists
+        """
+        pass
+
+    @abstractmethod
+    def visit(self, vertex):
+        """
+        set visited flag on vertex
+        """
+        pass
+
+    @abstractmethod
+    def visit_edge(self, here, there):
+        """
+        set visited flag on edge
+        """
+        pass
+
+    @abstractmethod
+    def visited(self, vertex):
+        """
+        returns true if vertex has been visited
+        """
+        pass
+
+    @abstractmethod
+    def visited_edge(self, here, there):
+        """
+        returns true if edge has been visited
+        """
+        pass
+
+    @abstractmethod
+    def reset(self):
+        """
+        set all visited flags to False
+        """
+        pass
+
+    @abstractmethod
+    def degree(self, vertex):
+        """
+        return number of vertices adjacent to vertex
+        """
+        pass
+
+    @abstractmethod
+    def __iter__(self):
+        """
+        iterate across all vertices of graph
+        """
+        pass
+
+    @abstractmethod
+    def neighbors(self, value):
+        """
+        return all vertices adjacent to vertex
+        """
+        pass
+
+    @abstractmethod
+    def edges(self):
+        """
+        return all edges in Graph
+        """
+        pass
+
+    @abstractmethod
     def is_empty(self):
         """
         check if Graph is empty
         """
         pass
 
-    @staticmethod
-    def adjacent(vertex, other):
+    @abstractmethod
+    def clear(self):
+        """
+        remove all vertices from Graph
+        """
+        pass
+
+    @abstractmethod
+    def adjacent(self, vertex, other):
         """
         check if vertex and other have an edge between them
         """
--- Graphs/adjacency_matrix_graph.py
+++ Graphs/adjacency_matrix_graph.py
@@ -4,8 +4,306 @@ from Graphs._graph_abstract import Graph
 
 class AdjacencyMatrixGraph(Graph):
     """
-    implementing adjaceny matrix graph
+    implementing adjacency matrix graph
     """
+    class Vertex(Graph.Vertex):
+        """
+        vertex class for matrix Graph
+        """
+        def __init__(self, value, index):
+            """
+            override with index property
+            """
+            super().__init__(value)
+            self.index = index
 
-    def __init__(self, vertices, edges):
-        self.
+    def __init__(self, vertices=None, edges=None, directed=False):
+        self.directed = directed
+        self.size = 0
+        self.capacity = 0
+        self.edge_count = 0
+        self.matrix = []
+        self.vertex_dict = {}
+        self.free = set()
+        if vertices:
+            for vertex in vertices:
+                self.add_vertex(vertex)
+        if edges:
+            for edge in edges:
+                if len(edge) == 2:
+                    self.add_edge(edge[0], edge[1])
+                elif len(edge) == 3:
+                    self.add_edge(edge[0], edge[1], edge[2])
+                else:
+                    raise KeyError("Incorrect parameters")
+
+    def __len__(self):
+        return self.size
+
+    def __repr__(self):
+        return str(self.matrix)
+
+    def add_vertex(self, value):
+        """
+        add vertex with value value to Graph
+        """
+        if value not in self:
+            if len(self.free) != 0:
+                index = self.free.pop()
+            else:
+                index = len(self)
+                self.capacity += 1
+                for there in self.matrix:
+                    there.append(None)
+                self.matrix.append([None] * self.capacity)
+            self.size += 1
+            vertex = self.Vertex(value, index)
+            self.vertex_dict[value] = vertex
+
+    def delete_vertex(self, value):
+        """
+        delete vertex with value value from Graph
+        """
+        if value in self:
+            index = self.vertex_dict[value].index
+            count = 0
+            for x in range(len(self.matrix[index])):
+                if self.matrix[index][x]:
+                    count += 1
+                    self.matrix[index][x] = None
+            self.edge_count = self.edge_count - count
+            count = 0
+            for there in self.matrix:
+                if there[index]:
+                    count += 1
+                    there[index] = None
+            if self.directed:
+                self.edge_count = self.edge_count - count
+            self.size -= 1
+            del self.vertex_dict[value]
+        else:
+            return "Vertex not in Graph"
+
+    def add_edge(self, here, there, label=None):
+        """
+        add edge from here to there to Graph
+        add vertices here and there in not in Graph
+        """
+        if not here in self:
+            self.add_edge(here)
+        if not there in self:
+            self.add_edge(there)
+        here_vertex = self.vertex_dict[here]
+        there_vertex = self.vertex_dict[there]
+        new_edge = self.Edge(here_vertex.value, there_vertex.value, label, directed=self.directed)
+        if not self.contains_edge(here, there):
+            self.edge_count += 1
+        self.matrix[here_vertex.index][there_vertex.index] = new_edge
+        if not self.directed:
+            self.matrix[there_vertex.index][here_vertex.index] = new_edge
+
+    def delete_edge(self, here, there):
+        """
+        delete edge from here to there from Graph
+        """
+        if self.contains_edge(here, there):
+            here_vertex = self.vertex_dict[here]
+            there_vertex = self.vertex_dict[there]
+            old_edge = self.matrix[here_vertex.index][there_vertex.index]
+            self.matrix[here_vertex.index][there_vertex.index] = None
+            if not self.directed:
+                self.matrix[there_vertex.index][here_vertex.index] = None
+            self.edge_count -= 1
+            return old_edge.label
+        else:
+            return "Edge not in Graph"
+
+    def get_vertex(self, value):
+        """
+        get vertex with value value
+        """
+        if value in self:
+            return self.vertex_dict[value]
+
+    def get_edge(self, here, there):
+        """
+        get edge from here to there
+        """
+        if self.contains_edge(here, there):
+            here_vertex = self.vertex_dict[here]
+            there_vertex = self.vertex_dict[there]
+            return self.matrix[here_vertex.index][there_vertex.index]
+
+    def __contains__(self, value):
+        """
+        check if vertex with value value in Graph
+        """
+        return value in self.vertex_dict
+
+    def contains_edge(self, here, there):
+        """
+        check if edge from here to there in Graph
+        """
+        if here in self and there in self:
+            here_vertex = self.vertex_dict[here]
+            there_vertex = self.vertex_dict[there]
+            return self.matrix[here_vertex.index][there_vertex.index]
+        return False
+
+    def visit(self, value):
+        """
+        visit vertex with value value
+        """
+        if value in self:
+            vertex = self.vertex_dict[value]
+            return vertex.visit()
+
+    def visit_edge(self, here, there):
+        """
+        visit edge from here to there
+        """
+        if here in self and there in self:
+            here_vertex = self.vertex_dict[here]
+            there_vertex = self.vertex_dict[there]
+            edge = self.matrix[here_vertex.index][there_vertex.index]
+            # if not self.directed:
+            #     opposite = self.matrix[there_vertex.index][here_vertex.index]
+            #     opposite.visit()
+            return edge.visit()
+
+    def visited(self, value):
+        """
+        check if vertex had been visited
+        """
+        if value in self:
+            vertex = self.vertex_dict[value]
+            return vertex.visited()
+
+    def visited_edge(self, here, there):
+        """
+        check if edge from here to there has been visited
+        """
+        if here in self and there in self:
+            here_vertex = self.vertex_dict[here]
+            there_vertex = self.vertex_dict[there]
+            edge = self.matrix[here_vertex.index][there_vertex.index]
+            return edge.visited()
+
+    def reset(self):
+        """
+        reset visited flag on all edges and vertices
+        """
+        for vertex in self.vertex_dict:
+            vertex.reset()
+            row = self.matrix[vertex.index]
+            for edge in [e for e in row if e is not None]:
+                edge.reset()
+
+    def is_empty(self):
+        """
+        check if Graph is empty
+        """
+        return self.size == 0
+
+    def __iter__(self):
+        for vertex in self.vertex_dict:
+            yield vertex
+
+    def neighbors(self, value):
+        """
+        return all vertices adjacent to vertex with value value
+        """
+        return self.Neighbors(self, value)
+
+    def visitable(self, value):
+        """
+        return all vertices vistable from vertex with value value using DFS
+        """
+        visited, stack = set(), [value]
+        while stack:
+            vertex = stack.pop()
+            if vertex not in visited:
+                visited.add(vertex)
+                stack.extend(set(self.neighbors(vertex)) - visited)
+        return visited - set([value])
+
+    def edges(self):
+        """
+        return all edges in Graph
+        """
+        return self.Edges(self)
+
+    def degree(self, value):
+        """
+        return number of vertices adjacent to vertex with value value
+        """
+        if value in self:
+            vertex = self.vertex_dict[value]
+            row = self.matrix[vertex.index]
+            count = len([edge for edge in row if edge is not None])
+            return count
+
+    def clear(self):
+        """
+        clear Graph of contents
+        """
+        self.size = 0
+        self.capacity = 0
+        self.edge_count = 0
+        self.matrix = []
+        self.vertex_dict = {}
+        self.free = set()
+
+    def adjacent(self, value, other):
+        """
+        check if vertices with value value and other have an edge between them
+        """
+        if value in self:
+            vertex = self.vertex_dict[value]
+            row = self.matrix[vertex.index]
+            for edge in [edge for edge in row if edge is not None]:
+                if edge.here == other or edge.there == other:
+                    return True
+        return False
+
+    class Neighbors:
+        """
+        iterable for neighbors of vertex
+        """
+        def __init__(self, target, value):
+            self.target = target
+            self.value = value
+
+        def __iter__(self):
+            g = self.target
+            if self.value in g:
+                vertex = g.vertex_dict[self.value]
+                row = g.matrix[vertex.index]
+                for edge in [e for e in row if e is not None]:
+                    if edge.here != self.value:
+                        yield edge.here
+                    else:
+                        yield edge.there
+
+        def __len__(self):
+            return self.target.degree(self.value)
+
+    class Edges:
+        """
+        iterable for edges in Graph
+        """
+        def __init__(self, target):
+            self.target = target
+
+        def __iter__(self):
+            vertices = self.target.vertex_dict.values()
+            for here in vertices:
+                row = self.target.matrix[here.index]
+                for there in vertices:
+                    if self.target.directed or (there.index >= here.index):
+                        edge = row[there.index]
+                        if edge:
+                            yield edge
+
+        def __len__(self):
+            return self.target.edge_count
